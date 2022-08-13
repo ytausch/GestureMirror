562abade1fc12a09a58a813253cdece769f4e587
Yannik Tausch <dev@ytausch.de>
2022-08-13 20:13:53 +0200

    finish README, update main

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # GestureMirror
-Using an APDS9960 gesture sensor and a Raspberry Pi, we control an external monitor and a SONOS speaker.
+Using an APDS9960 gesture sensor and a Raspberry Pi, this project can control an external display and a Sonos speaker.
+
+This project might be handy if you happen to have a Raspberry Pi [smart mirror](https://magicmirror.builders/) that
+is connected to an APDS9960 gesture sensor and nearby a Sonos speaker and fits use cases like the following:
+
+> The display should turn on when you make a gesture and should automatically turn off when a timer expires.
+Another gesture should turn on the speaker and play from a predefined URI (e. g. a radio station).
+
+## Requirements
+* Python 3.7+ (lower 3.x versions might work)
+* The setup described above.
+
+## Usage
+First, clone this repository and install the dependencies (you are advised to use
+[virtual environments](https://docs.python.org/3/library/venv.html)):
+```bash
+git clone https://github.com/ytausch/GestureMirror.git
+cd GestureMirror
+pip install -r requirements.txt
+```
+
+Then, make sure to personalize `main.py` for your needs.
+
+Finally, start the application:
+```bash
+python3 ./src/gesturemirror/main.py
+```
--- src/gesturemirror/main.py
+++ src/gesturemirror/main.py
@@ -1,31 +1,40 @@
 import time
+
 import smbus
 
 from gesturemirror.display_switcher import DisplaySwitcher
+from gesturemirror.gesture_controller import GestureController
 from gesturemirror.gesture_sensor import GestureSensor
+from gesturemirror.sonos_controller import SonosController
 
 I2C_PORT = 1
+
+# the rotation of the APDS9960 sensor (counter-clockwise, in degrees)
 ROTATION = 180
+SONOS_IP = "192.168.2.10"   # set your Sonos IP here
+
+RADIO_URL = "SET_YOUR_RADIO_URL_HERE"
+RADIO_TITLE = "SET_YOUR_RADIO_TITLE_HERE"
 
 
 def main():
     i2c = smbus.SMBus(I2C_PORT)
     sensor = GestureSensor(i2c, ROTATION)
+    gesture_controller = GestureController(sensor)
 
-    switcher = DisplaySwitcher()
+    display = DisplaySwitcher()
+    sonos = SonosController(SONOS_IP)
+
+    def play_radio():
+        sonos.play_uri(RADIO_URL, RADIO_TITLE, force_radio=True)
+
+    gesture_controller.register_gesture_action(GestureSensor.GESTURE_UP, lambda: display.turn_on())
+    gesture_controller.register_gesture_action(GestureSensor.GESTURE_LEFT, play_radio)
+    gesture_controller.register_gesture_action(GestureSensor.GESTURE_RIGHT, lambda: sonos.stop())
 
     while True:
-        gesture = sensor.gesture()
-        if gesture == GestureSensor.GESTURE_UP:
-            print("Up")
-            switcher.turn_on()
-        elif gesture == GestureSensor.GESTURE_DOWN:
-            print("Down")
-            switcher.turn_off()
-        elif gesture == GestureSensor.GESTURE_LEFT:
-            print("Left")
-        elif gesture == GestureSensor.GESTURE_RIGHT:
-            print("Right")
+        gesture_controller.update()
+        display.timeout_update()
         time.sleep(0.5)
 
 
